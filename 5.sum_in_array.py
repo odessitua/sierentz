6aa26754e5762ba46eeec036ef9f0a925a893d35
odessitua <85794411+odessitua@users.noreply.github.com>
2022-10-22 08:02:34 -0400

    Update 5.sum_in_array.py

--- 5.sum_in_array.py
+++ 5.sum_in_array.py
@@ -20,6 +20,24 @@ def sum_in_array(sorted_array):
 
 print(sum_in_array(sorted_array))
 
+#v1.1 optimize
+def sum_in_array_1_1(sorted_array):
+    left=0
+    right=len(sorted_array)-1
+    while left<right:
+        my_sum = sorted_array[left]+sorted_array[right]
+        if my_sum == S:
+            #stop when find first
+            return [sorted_array[left],sorted_array[right]]
+        if my_sum > S:
+            right-=1
+        else:
+            left+=1
+        
+    return [-1]
+
+print(sum_in_array_1_1(sorted_array))
+
 #v2
 def sum_in_array2(sorted_array):
     for key,i in enumerate(sorted_array):
